9b2e5029b6c248d0af3a614fed383d1049734ced
PRINCE <97717613+prince0010@users.noreply.github.com>
2023-09-17 17:18:55 +0800

    first commit

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,70 @@
+<aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 " id="sidenav-main">
+    <div class="sidenav-header">
+      <a class="navbar-brand m-0">
+        <h4 >Bureau of Fire Protection</h4>
+      </a>
+    </div>
+    <hr class="horizontal dark mt-0">
+    <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link  active" href="index.php">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class = "fa fa-home text-dark text-lg"></i>
+            </div>
+            <span class="nav-link-text ms-1">Dashboard</span>
+          </a>
+        </li>
+
+        <li class="nav-item mt-3">
+          <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Enquiries</h6>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link  " href="enquiries.php">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+            <i class = "fa fa-bullhorn text-dark text-lg"></i>
+            </div>
+            <span class="nav-link-text ms-1">Enquiries</span>
+          </a>
+        </li>
+     
+        <li class="nav-item mt-3">
+          <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Manage Services</h6>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link  " href="services.php">
+                <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+                    <i class = "fa fa-cogs text-dark text-lg"></i>
+                </div>
+                <span class="nav-link-text ms-1">Services</span>
+            </a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link  " href="enquiries.php">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+            <i class = "fa fa-user-plus text-dark text-lg"></i>
+            </div>
+            <span class="nav-link-text ms-1">Admin/User</span>
+          </a>
+        </li>
+        
+        <li class="nav-item mt-3">
+          <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Setting</h6>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link  " href="setting.php">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+            <i class = "fa fa-globe text-dark text-lg"></i>
+            </div>
+            <span class="nav-link-text ms-1">Setting</span>
+          </a>
+        </li>
+       
+      </ul>
+    </div>
+    <div class="sidenav-footer mx-3 ">
+      <a class="btn bg-dark mt-3 w-100" href = "logout.php"> Logout </a>
+    </div>
+   
+  </aside>
---
+++ admin/index.php
@@ -0,0 +1,33 @@
+<?php include('includes/header.php');?>
+
+<div class="row">
+<div class="col-md-3 mb-4">
+          <div class="card card-body p-3">
+                    <p class="text-sm mb-0 text-capitalize font-weight-bold">Users</p>
+                    <h5 class="font-weight-bolder mb-0">
+                      2
+                    </h5>
+                </div>
+          </div>
+
+          <div class="card card-body p-3">
+                    <p class="text-sm mb-0 text-capitalize font-weight-bold">Something</p>
+                    <h5 class="font-weight-bolder mb-0">
+                      2
+                    </h5>
+                </div>
+          </div>
+
+          <div class="card card-body p-3">
+                    <p class="text-sm mb-0 text-capitalize font-weight-bold">Something</p>
+                    <h5 class="font-weight-bolder mb-0">
+                      2
+                    </h5>
+                </div>
+          </div>
+
+
+</div>
+
+
+<?php include('includes/footer.php');?>
